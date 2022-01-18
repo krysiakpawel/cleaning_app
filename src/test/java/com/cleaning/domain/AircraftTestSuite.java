e98643480c7344f4373245be603f64c51e276347
krysiakpawel <krysiak.p@o2.pl>
2022-01-18 16:31:02 +0000

    Fixed DB connection
    Fixed "Public Key Retrieval Not Allowed"

--- src/test/java/com/cleaning/domain/AircraftTestSuite.java
+++ src/test/java/com/cleaning/domain/AircraftTestSuite.java
@@ -1,8 +1,12 @@
 package com.cleaning.domain;
 
 import com.cleaning.domain.aircraft.Aircraft;
-import com.cleaning.domain.aircraft.dao.AircraftDao;
+import com.cleaning.domain.airport.Airport;
+import com.cleaning.domain.turnaround.Turnaround;
 import com.cleaning.service.AircraftService;
+import com.cleaning.service.AirportService;
+import com.cleaning.service.TurnaroundService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +20,10 @@ public class AircraftTestSuite {
 
     @Autowired
     private AircraftService aircraftService;
+    @Autowired
+    private AirportService airportService;
+    @Autowired
+    private TurnaroundService turnaroundService;
 
 
     @Test
@@ -25,8 +33,30 @@ public class AircraftTestSuite {
         Aircraft aircraft = new Aircraft("TF-ISO", "767-300");
         //When
         aircraftService.saveAircraft(aircraft);
-
+        //Then
 
     }
 
+    @Test
+    public void testAirportDaoSave(){
+
+        //Given
+        Airport airport = new Airport("ORD", "Chicago", "USA");
+
+        //When
+        airportService.saveAirport(airport);
+
+    }
+
+    @Test
+    public void testTurnaroundDaoSave(){
+        //Given
+        Airport airportORD = new Airport("ORD", "Chicago", "USA");
+        Airport airportTXL = new Airport("TXL", "Berlin", "Germany");
+        Aircraft aircraft = new Aircraft("TF-ISO", "767-300");
+        Turnaround turnaround = new Turnaround(aircraft, airportTXL, airportORD );
+        //When
+        turnaroundService.save(turnaround);
+
+    }
 }
