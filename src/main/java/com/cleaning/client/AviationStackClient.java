1c20bcbab4fb98c76fdf1dcbc4bad172a5782a41
krysiakpawel <krysiak.p@o2.pl>
2022-01-18 16:31:29 +0000

    Hashed classes

--- src/main/java/com/cleaning/client/AviationStackClient.java
+++ src/main/java/com/cleaning/client/AviationStackClient.java
@@ -1,35 +1,36 @@
-package com.cleaning.client;
-
-
-import com.cleaning.domain.aircraft.AircraftDto;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-
-@Component
-public class AviationStackClient {
-
-    @Value("${aviationstack.endpoint.api.prod}")
-    private String aviationStackEndpoint;
-
-    @Value("${aviationstack.app.key}")
-    private String aviationStackKey;
-
-
+//package com.cleaning.client;
+//
+//
+//import com.cleaning.domain.aircraft.AircraftDto;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.client.RestTemplate;
+//import org.springframework.web.util.UriComponentsBuilder;
+//
+//import java.net.URI;
+//
+//@Component
+//public class AviationStackClient {
+//
+//    @Value("${aviationstack.endpoint.api.prod}")
+//    private String aviationStackEndpoint;
+//
+//    @Value("${aviationstack.app.key}")
+//    private String aviationStackKey;
+//
+//
 //    @Autowired
-    private RestTemplate restTemplate;
-
-
-//    public AircraftDto[] getData() {
-//        URI uri = UriComponentsBuilder.fromHttpUrl(aviationStackEndpoint + "flights")
-//                .queryParam("access_key", aviationStackKey)
-//                .queryParam("arr_iata", "kef")
-//                .queryParam("airline_name", "icelandair")
-//                .queryParam("flight_status", "landed").build().encode().toUri();
-
-//        Data data = restTemplate.getForObject(uri, Data.class);
-//        return data.getAircraftDtoList();
-    }
+//    private RestTemplate restTemplate;
+//
+//
+////    public AircraftDto[] getData() {
+////        URI uri = UriComponentsBuilder.fromHttpUrl(aviationStackEndpoint + "flights")
+////                .queryParam("access_key", aviationStackKey)
+////                .queryParam("arr_iata", "kef")
+////                .queryParam("airline_name", "icelandair")
+////                .queryParam("flight_status", "landed").build().encode().toUri();
+//
+////        Data data = restTemplate.getForObject(uri, Data.class);
+////        return data.getAircraftDtoList();
+//    }
